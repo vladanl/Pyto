8a4079a56603ed9cdcdff88697aabdef0eebe20b
Benjamin Barad <benjamin.barad@gmail.com>
2020-06-12 14:10:08 -0700

    Create setup.py
    minimal setup.py enables installation w/ pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyto",
+    version="1",
+    author="Vladan Lucic",
+    author_email="vladan@biochem.mpg.de",
+    packages=find_packages(),
+    python_requires=">=3.0",
+    include_package_data=True
+)
