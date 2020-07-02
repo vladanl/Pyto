0fa3aad96b96539ba1e88f6688a3ff60f9f2bfa7
Benjamin Barad <benjamin.barad@gmail.com>
2020-07-02 14:26:24 -0700

    Update setup.py
    Added requires

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ setup(
     author_email="vladan@biochem.mpg.de",
     packages=find_packages(),
     python_requires=">=3.0",
+    install_requires=[
+        "numpy>=1.18",
+        "scipy>=1.41",
+        "matplotlib>=3.2",
+        "pandas>=1.0"
+    ],
     include_package_data=True
 )
