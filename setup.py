35f87577a217dbe35cd90bc14c7a5c291ceb8e08
Vladan Lucic <vladan@biochem.mpg.de>
2021-02-08 17:54:26 +0100

    Files for packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as readme_fh:
+    long_description = readme_fh.read()
+
 setup(
     name="pyto",
-    version="1",
+    version="1.7",
     author="Vladan Lucic",
     author_email="vladan@biochem.mpg.de",
+    description=(
+        "Detection and analysis of pleomorphic membrane-bound "
+        + "molecular complexes in cryo-electron tomograms"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vladanl/Pyto",
     packages=find_packages(),
-    python_requires=">=3.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     install_requires=[
         "numpy>=1.18",
         "scipy>=1.41",
